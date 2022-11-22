a55bb62930f65920ca91dc425362189f7ec2d756
alecv <alec.thien-trung.q.vrancken@vub.be>
2022-11-22 13:27:05 +0100

    provisional code of the game

--- SPEL.asm
+++ SPEL.asm
@@ -58,7 +58,7 @@ PROC terminateProcess
 	ret
 ENDP terminateProcess
 
-;Procedure wait_VBLANK van EXAMPLES\DANCER genomen
+; Procedure wait_VBLANK van EXAMPLES\DANCER genomen
 ; wait for @@framecount frames
 PROC wait_VBLANK
 	ARG @@framecount: word
@@ -79,90 +79,57 @@ PROC wait_VBLANK
 	ret 
 ENDP wait_VBLANK
 
-PROC printUnsignedInteger
-	ARG	@@printval:dword    ; input argument
-	USES eax, ebx, ecx, edx
-
-	mov eax, [@@printval]
-	mov	ebx, 10		; divider
-	xor ecx, ecx	; counter for digits to be printed
-
-	; Store digits on stack
-	@@getNextDigit:
-		inc	ecx         ; increase digit counter
-		xor edx, edx
-		div	ebx   		; divide by 10
-		push dx			; store remainder on stack
-		test eax, eax	; check whether zero?
-		jnz	@@getNextDigit
-
-		; Write all digits to the standard output
-		mov	ah, 2h 		; Function for printing single characters.
-	@@printDigits:		
-		pop dx
-		add	dl,'0'      	; Add 30h => code for a digit in the ASCII table, ...
-		int	21h            	; Print the digit to the screen, ...
-		loop @@printDigits	; Until digit counter = 0.
-		
-	ret
-ENDP printUnsignedInteger
-
-PROC updatecolorpallete ;moet nog veranderd worden in a loop
+PROC updateColorpallete
 
 	USES eax, ebx, ecx, edx
 
-	mov ebx, offset paletteperso
+	mov ebx, offset palette
 
 	mov ah, 0
 
 	@@kleur:
-		mov DX, 03C8h ; DAC write port
+		mov DX, 03C8h 						; DAC write port
 		push eax
 		mov al, ah
-		out DX, Al ; write to IO
+		out DX, Al 							; write to IO
 		pop eax
 
 
-		mov DX, 03C9h ; DAC data port
+		mov DX, 03C9h 						; DAC data port
 
-		mov AL, [ebx] ; load red value (6-bit)
-		out DX, AL ; write red value
-
+		mov AL, [ebx] 						; load red value (6-bit)
+		out DX, AL 							; write red value
 		add ebx, 4
-		mov AL, [ebx] ; load green value (6-bit)
-		out DX, AL ; write green value
-
+		mov AL, [ebx] 						; load green value (6-bit)
+		out DX, AL 							; write green value
 		add ebx, 4
-		mov AL, [ebx] ; load blue value (6-bit)
-		out DX, AL ; write blue value
-
+		mov AL, [ebx] 						; load blue value (6-bit)
+		out DX, AL 							; write blue value
 		add ebx, 4
 		
-
 		inc ah
 		cmp ah, 5
 		jne @@kleur
 
 	ret
-ENDP updatecolorpallete
+ENDP updateColorpallete
 
 ; Fill the background (for mode 13h): blue sky with grass and a wall
 PROC fillBackground
 	USES 	eax, ecx, edi, edx, ebx
 
 	; Initialize video memory address.
-	mov	edi, VMEMADR ; edi is destination adress en is dus hier 0A0000h
+	mov	edi, VMEMADR 						; edi is destination adress en is dus hier 0A0000h
 
 
-	; lucht tekenen
-	; Scan the whole video memory and assign the background colour.
-	mov	ecx, SCRWIDTH*150 ; ecx = amount of elements = aantal pixels
-	mov	al, 0	; indx of the first color to change
+	; Draw sky
+	mov	ecx, SCRWIDTH*150 					; ecx = amount of elements = aantal pixels
+	mov	al, 0								; indx of the first color to change
 	rep	stosb			; stosb (byte) =transfer one byte from eax to edi so that edi increases/updates to point to the next datum(that is situated one byte next to the previous)
 						;stosw (word) = transfer two byte (= word)
 						;stosd (double word) = tranfer 4 bytes (= double word)
 
-	;gras tekenen
+	; Draw grass
 	mov	edi, VMEMADR
 	add edi, 150*320
 	mov al, 1
@@ -180,7 +147,7 @@ PROC fillBackground
 		cmp edx, 0
 		jne @@hoogte
 
-	;muur tekenen
+	; Draw wall
 	mov edx, 1
 	@@hoogtemuur:
 		xor ebx, ebx	
@@ -206,7 +173,7 @@ PROC fillBackground
 		cmp edx, 100
 		jne @@hoogtemuur
 
-	;doelwit tekenen
+	; Draw target
 	mov edx, 1
 	@@hoogtedoel:
 		xor ebx, ebx	
@@ -234,12 +201,12 @@ PROC fillBackground
 	ret
 ENDP fillBackground
 
+;write pixel in a standard (x,y) cartesian coordinate system with the origin far left above grond 
 PROC drawPixel
 	ARG @@xcoord:dword ,@@ycoord:dword, @@color:byte
 	USES eax, ebx
 
 	mov	edi, VMEMADR
-	;write pixel in ycoord lines down and xcoord lines to the right
 	mov eax, [@@ycoord]
 	mov ebx, 149
 	sub ebx, eax
@@ -254,6 +221,7 @@ PROC drawPixel
 	ret
 ENDP drawPixel
 
+; deletes previous bullet and draws a new one
 PROC moveBullet
 	ARG @@oldXpos:dword, @@oldYpos:dword, @@newXpos:dword, @@newYpos:dword
 	USES ebx, ecx
@@ -297,7 +265,7 @@ PROC moveBullet
 	dec ecx
 	call drawPixel, ebx, ecx, 4
 	inc ebx
-	call drawPixel, ebx, ecx, 4
+	call drawPixel, ebx, ecx, 3
 	inc ebx
 	call drawPixel, ebx, ecx, 4
 	sub ebx, 2
@@ -312,13 +280,13 @@ PROC moveBullet
 ENDP moveBullet
 
 ;procedure om de baan te berekenen
-PROC kogelbaan
+PROC bulletPath
 	ARG @@vxbegin:dword, @@vybegin:dword
 	LOCAL @@tijd:dword, @@dt:dword, @@xpos:dword, @@ypos:dword, @@vx:dword, @@vy:dword, @@ax:dword, @@ay:dword
 	USES eax, ebx
 
 	mov [@@tijd], 0
-	mov [@@dt], 1					; Eenheid:	[tijdseenheid]
+	mov [@@dt], 1					; [time unit]
 
 	mov eax, 25
 	mov ebx, 25
@@ -326,68 +294,79 @@ PROC kogelbaan
 	push eax
 	push ebx
 
-	mov [@@xpos], eax				;		   	[2^(-5)-ste van een pixels]
-	mov [@@ypos], ebx				; 		        ""          ""
-	mov eax, [@@vxbegin]			;          	[2^(-5)-ste van een pixels per tijdseenheid]
+	mov [@@xpos], eax				;[pixels]
+	mov [@@ypos], ebx				;[pixels]
+	mov eax, [@@vxbegin]			;[pixels/time unit]
 	mov [@@vx], eax
-	mov eax, [@@vybegin]			; 		        ""          ""	
+	mov eax, [@@vybegin]			;[pixels/time unit]
 	mov [@@vy], eax
-	mov [@@ax], 0					;          	[2^(-5)-ste van een pixels per tijdseenheid²]
-	mov [@@ay], 10					; NEGATIEF valversnelling (geen 9.81 want FP)	   ""     ""
+	mov [@@ax], 0					;[pixels/time unit²]
+	mov [@@ay], -10					;[pixels/time unit²] downward accelaration due to "gravity"
 
-	call	waitForSpecificKeystroke, 001Bh	; ESC = 001Bh
+
+	call moveBullet, [@@xpos], [@@ypos], [@@xpos], [@@ypos] 
+	call	waitForSpecificKeystroke, 001Bh
+
 	@@tijdsloop:
 		mov eax, [@@dt]
 		add [@@tijd], eax
 
-		;vx = ax*dt
+		;vx += ax*dt
 		mov eax, [@@ax]				; ik weet dat ax toch nul is maar in toekomst zal ook windkracht komen
 		mov ebx, [@@dt]				; ik weet ook dat dt toch 1 is (maar zou kunne veranderen) 
 		mul ebx
 		add [@@vx], eax
-		;vy = ay*dt
+		;vy += ay*dt
 		mov eax, [@@ay]
 		mov ebx, [@@dt]
 		mul ebx
-		sub [@@vy], eax
-		;xpos = vx*dt 
+		add [@@vy], eax
+		;xpos += vx*dt 
 		mov eax, [@@vx]
 		mov ebx, [@@dt]
 		mul ebx
 		add [@@xpos], eax
-		;ypos = vy*dt
+		;ypos += vy*dt
 		mov eax, [@@vy]
 		mov ebx, [@@dt]
 		imul ebx
 		add [@@ypos], eax
 		
-
-		pop ebx
+		;Bring back old coordinations
+		pop ebx						
 		pop eax
 
+		;bring back old coordinations
 		call moveBullet, eax, ebx, [@@xpos], [@@ypos] 
 
 		mov eax, [@@xpos]
 		mov ebx, [@@ypos]
+		
+		;Store new coordinations for next loop
 		push eax
 		push ebx
 
+		;Checks wall collision
+		cmp eax, 297							; (hou rekening met breedte kogel)
+		jge @@endWall
+		
+		;Checks ground collision
+		cmp ebx, 3								; (hou rekening met hoogte kogel)								
+		jle @@endGround
 
-		cmp eax, 297							; check muur (zal ongeveer 200 pixels verder zijn) 
-		jge @@einde								; (hou rekening met breedte kogel)
-
-		cmp ebx, 3								; check grondlimiet
-		jle @@einde								; (hou rekening met hoogte kogel)
-
-		call wait_VBLANK, 15					; 100 --> 1 seconde
+		call wait_VBLANK, 15					; [*10ms] animation purposes
 
 		jmp @@tijdsloop
 
-	@@einde:
+	@@endWall:
+			call moveBullet, eax, ebx, 297, ebx
+			jmp @@end
+	@@endGround:
+			call moveBullet, eax, ebx, eax, 2
 
+	@@end:
 	ret
-ENDP kogelbaan
-
+ENDP bulletPath
 
 
 PROC main
@@ -400,10 +379,11 @@ PROC main
 	call	setVideoMode, 13h
 	finit	; initialize FPU
 	
-	call	updatecolorpallete
-	call	fillBackground  ; black = (0,0,0) en white = (63, 63, 63)
+	call	updateColorpallete
+	call	fillBackground
 
-	call	kogelbaan, 45, 45
+	call	bulletPath, 45, 45
+	call	bulletPath, 25, 25
 
 	call	waitForSpecificKeystroke, 001Bh	; ESC = 001Bh
 	call	terminateProcess
@@ -412,15 +392,8 @@ ENDP main
 ; DATA
 ; -------------------------------------------------------------------
 DATASEG
-	
-	krachtwind dd 1		; de krachtwaarde van de wind
-	hoekwind dd 1		; de hoek van de wind
-	massa dd 1			; massa van voorwerp
-	v0 dd 8				; beginsnelheid van de worp
-	alpha dd 0.6 		; hoek van de worp
-	g dd 9.81
 
-	paletteperso dd 34, 52, 63, 31, 63, 0, 53, 26, 8, 55, 5, 15, 28, 32, 36				; lucht-gras-muur-doelwit-kogel
+	palette dd 34, 52, 63, 31, 63, 0, 53, 26, 8, 55, 5, 15, 28, 32, 36				; lucht-gras-muur-doelwit-kogel
 
 ; -------------------------------------------------------------------
 ; STACK
@@ -428,7 +401,3 @@ DATASEG
 STACK 100h
 
 END main
-
-
-
-;mfg
